49e1cd2d0924e5de21f71485c88e956bb157a2a6
Jordan Huizenga <jhuizenga99@gmail.com>
2022-04-11 05:53:18 +0000

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-wget -q https://apt.thoughtbot.com/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg
-echo "deb https://apt.thoughtbot.com/debian/ stable main" | tee /etc/apt/sources.list.d/thoughtbot.list
-apt-get update -y
-apt-get install -y rcm
+sudo apt-get update -y && sudo apt-get install -y vim wget
+sudo wget -q https://apt.thoughtbot.com/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg
+echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+sudo apt-get update -y
+sudo apt-get install -y rcm
 
 env RCRC=$HOME/dotfiles/rcrc rcup
 rcup
